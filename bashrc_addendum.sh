2f4e79d0ddbcbb9e38b43843095eaf7bd6058604
lgbrownjr <lgbrownjr@gmail.com>
2020-07-17 12:30:31 -0400

    initial committed version

---
+++ bashrc_addendum.sh
@@ -0,0 +1,9 @@
+#Add the following at the end to each user's .bashrc file on the pi:
+
+#If this login comes in from a bluetooth over serial line (rfcomm), then execute the following:
+if [ $(tty) = "/dev/rfcomm0" ] ; then
+	resize > /dev/null 2>&1 #Resize the screen.
+	source $HOME/bin/format.sh #Add this formatting script that allows the following line to get the pretty colors.
+	printf "\n${nor}Launching ${cyn}bluetooth${nor} to serial bridging utility...${end}\n" #notify the user that ser2bt_bridge is about to be launched
+	$HOME/bin/ser2bt_bridge #Launch ser2bt_bridge.
+fi
