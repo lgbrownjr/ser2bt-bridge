f21a51bf43b1efbb4d604801b5275d5e26594695
lgbrownjr <lgbrownjr@gmail.com>
2020-09-14 12:01:00 -0400

    put curley braces around variables

--- bashrc_addendum.sh
+++ bashrc_addendum.sh
@@ -3,8 +3,8 @@
 #If this login comes in from a bluetooth over serial line (rfcomm), then execute the following:
 
 
-#Main Variable declorations
-declare -a cmds=(resize stty ser2bt_bridge format.sh)
+#Main Variable & declorations
+declare -a cmds=(resize stty ser2bt_bridge)
 app_count=0
 installed_app_count=0
 source /usr/local/lib/format.sh
@@ -12,16 +12,16 @@ source /usr/local/lib/format.sh
 if [ $(tty) = "/dev/rfcomm0" ] ; then
 	for cmd in ${cmds[@]} ; do
 		let app_count++
-		if command -v $cmd > /dev/null 2>&1 ; then
+		if command -v ${cmd} > /dev/null 2>&1 ; then
 			let installed_app_count++
 	     else
-	     	printf "${red}$cmd${nor} not found, please use: \"${yel}sudo apt install $cmd${nor}\"\n"
+	     	printf "${red}${cmd}${nor} not found, please use: \"${yel}sudo apt install ${cmd}${nor}\"\n"
 	     fi
 	done
-	if [[ $app_count != $installed_app_count ]]; then
+	if [[ ${app_count} != ${installed_app_count} ]]; then
 		exit_status=3
 		exit_status_reason="${bro}Dependancies need to be met.${nor}\nExiting to shell"
-		exit $exit_status # Exit as dependancies need to be met.
+		exit ${exit_status} # Exit as dependancies need to be met.
 	fi
 
 #Check if the incomming connection is from rfcomm
