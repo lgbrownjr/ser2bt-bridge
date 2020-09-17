5535a8837d9566b3b0354dfdb32a8527c89b266f
lgbrownjr <lgbrownjr@gmail.com>
2020-09-17 14:06:35 -0400

    adding in formatting for better, more consistant experience.

---
+++ format.sh
@@ -0,0 +1,13 @@
+#color defenitions:
+drk_red=$'\e[00;31m'
+red=$'\e[01;31m'
+grn=$'\e[01;32m'
+yel=$'\e[01;33m'
+bro=$'\e[00;33m'
+drk_blu=$'\e[00;34m'
+blu=$'\e[01;34m'
+mag=$'\e[00;35m'
+cyn=$'\e[00;36m'
+end=$'\e[00;32m'
+nor=$'\e[01;37m'
+
