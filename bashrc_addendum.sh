01191fa7228067d40fbf0983591fe273bdccb55b
lgbrownjr <lgbrownjr@gmail.com>
2020-11-12 12:52:57 -0500

    changed the inactivity timeout to 30 minutes

--- bashrc_addendum.sh
+++ bashrc_addendum.sh
@@ -1,14 +1,14 @@
+#BEGIN ser2b-bridge .bashrc add-in
 #Add the following at the end to each user's .bashrc file on the pi:
 
 #If this login comes in from a bluetooth over serial line (rfcomm), then execute the following:
-
 
 #Main Variable & declorations
 source /usr/local/lib/format.sh
 declare -a cmds=(resize stty ser2bt_bridge)
 app_count=0
 installed_app_count=0
-TMOUT=300
+TMOUT=1800 #30 minutes
 
 #Check if the incomming connection is from rfcomm
 if [ $(tty) = "/dev/rfcomm0" ] ; then
@@ -26,10 +26,11 @@ if [ $(tty) = "/dev/rfcomm0" ] ; then
 		exit ${exit_status} # Exit as dependancies need to be met.
 	fi
 
-#Check if the incomming connection is from rfcomm
+#resize the screen, the export the inactivity timeout, then launch the ser2bt_bridge script:
 	resize > /dev/null 2>&1 #Resize the screen.
 	printf "\n${nor}Launching ${blu}bluetooth${nor} to serial bridging utility...${end}\n" #notify the user that ser2bt_bridge is about to be launched
 #	readonly TMOUT
-	export TMOUT #Exporting the auto logout timeout.
+	export TMOUT #Exporting the inactivity autologout timeout.
 	/usr/local/bin/ser2bt_bridge #Launch ser2bt_bridge.
 fi
+#END ser2b-bridge .bashrc add-in
