b11c72b7101c84ac47f7b37d5ae7ac5e60322b0e
lgbrownjr <lgbrownjr@gmail.com>
2020-11-27 15:05:50 -0500

    added text centering function

--- format.sh
+++ format.sh
@@ -15,3 +15,16 @@ whi=$'\e[01;37m'
 drk_whi=$'\e[00;37m'
 end=$'\e[0m'
 nor=$'\e[01;37m'
+
+#Formatting dunctions:
+function center (){
+  local ctrl_count="${1}"
+  local str="${2}"
+  local color_cost=8
+  local last_col=$(tput -T xterm cols)
+  let ctrl_cost=(${ctrl_count}*${color_cost})
+  let real_str_len=(${#str}+${ctrl_cost})
+  str=$(printf "%*s\n" $(((${real_str_len}+${last_col})/2)) "${str}")
+
+  echo "$str"
+}
