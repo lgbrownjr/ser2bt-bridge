bb0b09df527ca5bb6c9f437f3196e01101e794fc
lgbrownjr <lgbrownjr@gmail.com>
2020-12-16 15:24:10 -0600

    formatting changes

--- bashrc_addendum.sh
+++ bashrc_addendum.sh
@@ -17,18 +17,18 @@ if [ $(tty) = "/dev/rfcomm0" ] ; then
 		if command -v ${cmd} > /dev/null 2>&1 ; then
 			let installed_app_count++
 	     else
-	     	printf "${red}${cmd}${nor} not found, please use: \"${yel}sudo apt install ${cmd}${nor}\"\n"
+	     	printf "\n${red}${cmd}${nor} not found, please use: \"${yel}sudo apt install ${cmd}${nor}\"\n"
 	     fi
 	done
 	if [[ ${app_count} != ${installed_app_count} ]]; then
 		exit_status=3
-		exit_status_reason="${bro}Dependancies need to be met.${nor}\nExiting to shell"
+		exit_status_reason="\n${bro}Dependancies need to be met.${nor}\nExiting to shell"
 		exit ${exit_status} # Exit as dependancies need to be met.
 	fi
 
 #resize the screen, the export the inactivity timeout, then launch the ser2bt_bridge script:
 	resize > /dev/null 2>&1 #Resize the screen.
-	printf "${nor}Launching ${blu}bluetooth${nor} to serial bridging utility...${end}\n" #notify the user that ser2bt_bridge is about to be launched
+	printf "\n${nor}Launching ${blu}bluetooth${nor} to serial bridging utility...${end}\n" #notify the user that ser2bt_bridge is about to be launched
 #	readonly TMOUT
 	export TMOUT #Exporting the inactivity autologout timeout.
 	/usr/local/bin/ser2bt_bridge #Launch ser2bt_bridge.
